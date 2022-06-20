15fffd03d851c71b7f6f4c3e9a016f02f555ccde
Antonio <antarce3@gmail.com>
2022-06-19 22:56:08 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+main.remove()
+
+// const newHeader = document.createElement("h1");
+// <h1 id="victory">Antonio is the champion!</h1>
+// const newHeader = document.querySelector(`h1`)
+
+
+
+// document.newHeader.append('victory')
+// <newHeader id="victory"></newHeader>
+var newHeader = document.getElementById("victory")
+h1.textcontent = "Antonio is the champion"
+
+// var newHeader = document.createElement("h1")
+// newHeader.appendChild('victory')
+// newHeader.classList.add("victory")
+// document.body.newHeader.append(h1)
