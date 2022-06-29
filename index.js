685d7dabba6fdaf4586b3769ffdc5897f715ef0c
NotArtwork <AntArce3@gmail.com>
2022-06-29 09:27:50 -0400

    commit tree 5ef36eb02bf7fd7b0e4a1fe99bf07a2d8716d236

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 // Write your code here!
 main.remove()
 
-// const newHeader = document.createElement("h1");
-// <h1 id="victory">Antonio is the champion!</h1>
-// const newHeader = document.querySelector(`h1`)
-
-
-
-// document.newHeader.append('victory')
-// <newHeader id="victory"></newHeader>
-var newHeader = document.getElementById("victory")
-h1.textcontent = "Antonio is the champion"
-
-// var newHeader = document.createElement("h1")
-// newHeader.appendChild('victory')
-// newHeader.classList.add("victory")
-// document.body.newHeader.append(h1)
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id','victory')
+newHeader.setAttribute('class', 'victory')
+newHeader.textContent = "Antonio is the Champion!"
+document.getElementsByID(victory).textContent = "Antonio is the champion"
+const text = document.createTextNode('Antonio is the Champion!')
+newHeader.appendChild(text);
